6da52c0ccc6c731c60af134f03b811fd5827b334
Minh Rio <apple@Apples-MacBook-Pro.local>
2020-05-19 12:06:45 +0700

    initial

---
+++ resources/views/pages/registration.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Form đăng ký thành viên</title>
+</head>
+<body>
+    <form action="register.php" method="post">
+        <table>
+            <tr>
+                <td colspan="2">Form dang ky</td>
+            </tr>   
+            <tr>
+                <td>Username :</td>
+                <td><input type="text" id="username" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password :</td>
+                <td><input type="password" id="pass" name="pass"></td>
+            </tr>
+            <tr>
+                
+                <td>Phone :</td>
+                <td><input type="text" id="phone" name="phone"></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"><input type="submit" name="btn_submit" value="Dang ky"></td>
+            </tr>
+
+        </table>
+        
+    </form>
+</body>
+</html>
